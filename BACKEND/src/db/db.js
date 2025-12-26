3841b29f720a4e0da5940d4223e43f43819482ac
SUDHANSHU GHOSH <shudhanshukumar9713@gmail.com>
2025-12-26 14:54:59 +0530

    authentication almost complete

--- BACKEND/src/db/db.js
+++ BACKEND/src/db/db.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI)
+        .then(() => {
+            console.log("Connected to MongoDB");
+        })
     } catch (err) {
-        
+        console.error("Error connecting to MongoDB:", err);
     }
 }
 
---
+++ BACKEND/src/models/user.model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    name: {
+        type: String,
+        trim: true, // meaning no spaces before and after
+        required: true,
+    },
+    avatar: {
+        type: String,
+        default: "",
+        trim: true,
+    },
+    password: {
+        type: String,
+        minlength: 6,
+        required: true,
+    },
+    about: {
+        type: String,
+        default: "Hey there! I am using ConnectX.",
+        trim: true,
+    }
+    
+})
+
+const userModel = mongoose.model('users', userSchema);
+
+module.exports = userModel;
