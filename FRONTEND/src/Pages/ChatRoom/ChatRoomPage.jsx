0fe2a9a847b26a78069c0a6241ebb8bcb305b83e
SUDHANSHU GHOSH <shudhanshukumar9713@gmail.com>
2025-12-28 14:08:39 +0530

    almost completed the userList & chatroom UI.

--- FRONTEND/src/Pages/ChatRoom/ChatRoomPage.jsx
+++ FRONTEND/src/Pages/ChatRoom/ChatRoomPage.jsx
@@ -1,30 +1,43 @@
 import style from "./ChatRoomPage.module.scss";
+import { ArrowLeft, Video, Phone, Info, Send, Image } from "lucide-react";
+import userImg from "../../assets/dp_demo_img/doctor.jpg";
 
 const ChatRoomPage = () => {
   return (
     <div className={style.chatRoomPage}>
-      <div className="header">
-        <div className="back"></div>
-        <div className="pic"></div>
-        <div className="texts">
-          <p className="contactName">User Name</p>
-          <p className="status">Online</p>
+      <div className={style.header}>
+        <ArrowLeft />
+        <div className={style.participantDp}>
+          <img src={userImg} alt="userDP" />
         </div>
-        <div className="videoCall"></div>
-        <div className="voiceCall"></div>
-        <div className="info"></div>
-      </div>
-      <div className="fullMessage">
-        <div className="firstOne">
-            <p className="message">Hello there!</p>
+        <div className={style.texts}>
+          <p className={style.participantName}>User Name</p>
+          <p className={style.status}>Online</p>
         </div>
-        <div className="secondOne">
-            <p className="message">Hello there!</p>
+        <div className={style.userAction}>
+          <Video className={style.icon} />
+          <Phone className={style.icon} />
+          <Info className={style.icon} />
         </div>
       </div>
-      <form >
-        <input type="text" placeholder="Type a message" />
-        <button type="submit" className="send">send</button>
+      <div className={style.fullMessage}>
+        <div className={style.participantMsg}>
+          <p className={style.message}>Hello there!</p>
+          <div className={style.timeline}>11:49 PM</div>
+        </div>
+        <div className={style.yourMsg}>
+          <p className={style.message}>Hello there!</p>
+          <div className={style.timeline}>2:18 PM</div>
+        </div>
+      </div>
+      <form>
+        <div className={style.inputBox}>
+          <Image className={style.icon} />
+          <input type="text" placeholder="Type a message" />
+        </div>
+        <button type="submit" className="send">
+          <Send className={style.icon} />
+        </button>
       </form>
     </div>
   );
--- FRONTEND/src/Pages/UserList/UserList.jsx
+++ FRONTEND/src/Pages/UserList/UserList.jsx
@@ -1,20 +1,41 @@
 import style from "./UserList.module.scss";
-
+import AppIcon from '../../assets/AppIcon.png';
+import dpImg from '../../assets/dp_demo_img/doctor.jpg';
+import { EllipsisVertical, Search } from 'lucide-react';
 const UserList = () => {
   return (
-    <div>
-      <div className="header"></div>
-      <div className="searchSession"></div>
-      <div className="contactList">
-        <div className="contactOverview">
-          <div className="profilePic"></div>
-          <div className="texts">
-            <p className="contactName">User Name</p>
-            <p className="status">Online</p>
+    <div className={style.userListPage}>
+      <div className={style.header}>
+        <div className={style.logo}>
+          {/* <img src={AppIcon} alt="App Logo" /> */}
+          <p>ConnectX</p>
+        </div>
+        <div className={style.ownerActivity}>
+          <div className={style.profilePic}>
+            <img src={dpImg} alt="" />
           </div>
-          <div className="activity">
-            <p className="timeStamp">12:45 PM</p>
-            <div className="unreadCount">3</div>
+          <EllipsisVertical />
+        </div>
+      </div>
+      <div className={style.searchSession}>
+        <Search className={style.icon}/>
+        <input type="text" placeholder="Search or start new chat" />
+      </div>
+      <div className={style.participantList}>
+        <div className={style.participantOverview}>
+          <div className={style.profilePic}>
+            <img src={dpImg} alt="" />
+            <div className={style.statusIndicator}></div>
+          </div>
+          <div className={style.right}>
+            <div className={style.upper}>
+              <p className={style.participantName}>User Name</p>
+            <p className={style.DateStamp}>21/12/25</p>
+            </div>
+            <div className={style.down}>
+              <p className={style.lastMessage}>Last message preview goes here...</p>
+            <div className={style.unreadCount}>31</div>
+            </div>
           </div>
         </div>
       </div>
