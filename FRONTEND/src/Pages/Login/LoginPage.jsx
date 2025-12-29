8adbb4747e5d7825a2719c256135f8a59a69edd9
SUDHANSHU GHOSH <shudhanshukumar9713@gmail.com>
2025-12-29 21:23:54 +0530

    feat: enhance login page with form validation and loading state; add error messages for user feedback

--- FRONTEND/src/Pages/Login/LoginPage.jsx
+++ FRONTEND/src/Pages/Login/LoginPage.jsx
@@ -1,22 +1,42 @@
+import { useForm } from "react-hook-form";
 import style from "./LoginPage.module.scss";
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser } from "../../Redux/slices/authSlice";
 
 const LoginPage = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { loading } = useSelector(state => state.auth);
+
+  const onSubmit = async (data) => {
+    const result = await dispatch(loginUser(data));
+
+    if (loginUser.fulfilled.match(result)) {
+      navigate('/');
+    }
+  };
   return (
     <div className={style.loginPage}>
       <div className={style.upper}>
         <h1>Login</h1>
       </div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className={style.inputBox}>
           <i className={`ri-mail-fill ${style.icon}`}></i>
-          <input type="email" placeholder="Email" />
+          <input type="email" placeholder="Email"
+          {...register('email', { required: 'Email is required' })} />
         </div>
+        {errors.email && <span className={style.errors}>{errors.email.message}</span>}
         <div className={style.inputBox}>
           <i className={`ri-lock-2-fill ${style.icon}`}></i>
           <input type="password" placeholder="Password" />
         </div>
-        <button type="submit">Login</button>
+        {errors.password && <span className={style.errors}>{errors.password.message}</span>}
+        <button type="submit" disabled={loading}>
+          {loading ? 'Logging in...' : 'Login'}
+        </button>
         <div className={style.redirect}>
           <p>New to ConnectX? <Link to='/register'>Start connecting</Link></p>
         </div>
