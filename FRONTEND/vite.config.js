562a9148a94bd43f9452c01390807e861cc482e4
SUDHANSHU GHOSH <shudhanshukumar9713@gmail.com>
2026-03-01 22:26:24 +0530

    pwa integration on

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate', //Automatically updates the service worker in the bg
+      includeAssets: ['favicon-32x32.png', 'favicon-16x16.png', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'connectX',
+        short_name: 'connectX',
+        description: 'connectX | chat with anyone',
+        theme_color: '#2579ff',
+        background_color: '#2579ff',
+        display: 'standalone',
+        icons: [
+        ]
+      },
+      workbox: {
+        // Caches JS, CSS, HTML, and images for offline support
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest,mp3,jpg,jpeg}']
+      }
+    })
+  ],
 })
